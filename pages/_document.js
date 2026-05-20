0fd880f3b1776c33e5ccbb222fb1062afb48f795
hr-kairos <hr@kairosglobalsolutions.com>
2026-05-20 11:47:42 +0530

    Add custom Document component for Next.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      </Head>
+      <body className="bg-gray-50 text-gray-800 font-sans">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
