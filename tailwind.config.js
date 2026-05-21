73a918b4ffeb2f174070e0b226f47153294851cf
hr-kairos <hr@kairosglobalsolutions.com>
2026-05-21 10:07:04 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,7 @@ module.exports = {
         brand: {
           cyan: '#06b6d4',
           green: '#22c55e',
-          dark: '#0f172a'
-        }
-      },
-      animation: {
-        'marquee': 'marquee 25s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          dark: '#030712'
         }
       }
     },
