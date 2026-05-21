9680ad173da4cb6f18c14c4d5d1764e2fedc4123
hr-kairos <hr@kairosglobalsolutions.com>
2026-05-21 10:12:14 +0530

    Update send-email.js

--- pages/api/send-email.js
+++ pages/api/send-email.js
@@ -26,6 +26,6 @@ export default async function handler(req, res) {
     res.status(200).json({ success: true });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal pipeline error' });
+    res.status(500).json({ error: 'Internal pipeline fault' });
   }
 }
